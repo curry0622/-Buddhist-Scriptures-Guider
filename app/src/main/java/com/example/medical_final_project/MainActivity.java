f65ba4b161dfd71c2636cc4d2526fc3698f9bd54
curry0622 <58351529+curry0622@users.noreply.github.com>
2020-12-27 18:31:40 +0800

    feat: start page

--- app/src/main/java/com/example/medical_final_project/MainActivity.java
+++ app/src/main/java/com/example/medical_final_project/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.medical_final_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button startBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // my functions
+        findObjects();
+        btnOnClick();
+    }
+
+    public void findObjects() {
+        startBtn = findViewById(R.id.startBtn);
+    }
+
+    public void btnOnClick() {
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, MainMenu.class);
+                startActivity(intent);
+            }
+        });
     }
 }
